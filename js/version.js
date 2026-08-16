8b6922112c782f39d9c366891706eb69c5fcda2a
wcoatp <wcoatp@gmail.com>
2026-08-16 10:45:21 +0800

    chore: release app version 2026.08.16.2

--- js/version.js
+++ js/version.js
@@ -2,7 +2,7 @@
 (function exposeEchoVersion(root) {
   "use strict";
 
-  const app = "2026.08.16.1";
+  const app = "2026.08.16.2";
   const version = Object.freeze({
     app,
     // Derive the shell generation from the visible release. It is impossible
